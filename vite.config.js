ca0781208a427bf405203517a3dfb39bb831c46f
Marcos Oliveira <marcosluz312@hotmail.com>
2023-05-13 00:48:39 -0300

    vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import {resolve} from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const root = resolve(__dirname, 'public')
+const outDir = resolve(__dirname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir,
+    emptyOutDir: false,
+    rollupOptions:{
+      input:{
+        main: resolve(root,'index.html')
+      }
+    }
+  },
 })
